20541e8646e683f9bc15cec8b355dea58ea58b2b
Maxim <max4kaz@gmail.com>
2022-07-07 22:48:48 +0700

    Add all Tasks

---
+++ Task010_OutputSecondNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+Console.WriteLine("Введите трехзначное число ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+if(threeDigitNumber > 99 && threeDigitNumber < 1000)
+{
+    int result = threeDigitNumber / 10 % 10;
+    Console.WriteLine($"{threeDigitNumber} -> {result} - вторая цифра этого числа");
+}
+else
+{
+    Console.WriteLine("Вы нарушили запрос");
+}
---
+++ Task013_OutputThreeNumberOrNo/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите любое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number > 99 && number < 999)
+{
+    int result = number % 10;
+    Console.WriteLine($"{number} -> {result} - третья цифра этого числа");
+}
+else
+{
+    Console.WriteLine($"{number} -> третьей цифры нет у этого числа");
+}   
+
+   
+//   number = number / 10;
+
